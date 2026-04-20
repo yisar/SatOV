4fb6b312c9c09e97d5e460afb3c844fed7d0f039
Yisar <1533540012@qq.com>
2026-04-20 17:24:49 +0800

    emmm

--- benchmark/maskclip.py
+++ benchmark/maskclip.py
@@ -172,6 +172,6 @@ if __name__ == "__main__":
         'tree', 'grass', 'cropland', 'building'
     ]
     try:
-        run_inference("./dataset/DDOA/P2798.png", target_labels,"./benchmark/DDOA/maskclip/P2798.png")
+        run_inference("./dataset/UDD/DJI_0591.JPG", target_labels,"./benchmark/UDD/maskclip/DJI_0591.JPG")
     except FileNotFoundError:
         print("❌ 找不到图片，请检查 img2.jpg 是否在当前目录下。")
