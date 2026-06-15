910f47fd4b81b2641d9b8d20ebf6e9c99d80e893
yisar <1533540012@qq.com>
2026-06-16 00:00:39 +0800

    emmm

--- infer.py
+++ infer.py
@@ -73,7 +73,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     default_device = "cuda" if torch.cuda.is_available() else "cpu"
     parser.add_argument("--device", type=str, default=default_device)
-    parser.add_argument("--filename", type=str, default="./asset/img2.jpg")
+    parser.add_argument("--filename", type=str, default="./asset/img.jpg")
     parser.add_argument("--window_size", type=int, default=224, help="CLIP 窗口大小")
     parser.add_argument(
         "--stride", type=int, default=112, help="步长，推荐窗口的一半实现重叠"
@@ -182,7 +182,7 @@ def main():
 
         # 2. 执行全局 Dense CRF 优化 (核心步骤)
         # print(">>> 正在运行全局 Dense CRF 优化，请稍候...")
-        probs_np = apply_dense_crf(img_np, probs_np)
+        # probs_np = apply_dense_crf(img_np, probs_np)
 
         # 3. 最终类别判定
         max_idx = probs_np.argmax(axis=0)
