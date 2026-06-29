2eeac7cac19cf66d61ef971593af486c981dde21
yisar <1533540012@qq.com>
2026-06-29 14:36:17 +0800

    emmm

---
+++ util/dinov3.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+from PIL import Image
+import timm
+import torch
+
+img = Image.open(urlopen(
+    'https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/beignets-task-guide.png'
+))
+
+model = timm.create_model('convnext_tiny.dinov3_lvd1689m', pretrained=True)
+model = model.eval()
+
+# get model specific transforms (normalization, resize)
+data_config = timm.data.resolve_model_data_config(model)
+transforms = timm.data.create_transform(**data_config, is_training=False)
+
+output = model(transforms(img).unsqueeze(0))  # unsqueeze single image into batch of 1
+
+top5_probabilities, top5_class_indices = torch.topk(output.softmax(dim=1) * 100, k=5)
